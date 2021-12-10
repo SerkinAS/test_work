750c60c918ccd0015c0c102ff692aed34a1ca9a2
Алексей Серкин <69152024+SerkinAS@users.noreply.github.com>
2021-12-10 12:53:48 +0300

    Delete shortened_url directory

--- shortened_url/urls.py
+++
@@ -1,9 +0,0 @@
-"""Url's приложения shortened_url."""
-from django.urls import path
-from shortened_url.views import UrlShortView, UrlShortDetail
-
-
-urlpatterns = [
-    path('urls/', UrlShortView.as_view()),
-    path('urls/<int:pk>', UrlShortDetail.as_view())
-]
